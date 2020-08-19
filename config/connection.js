5337618e34e62ec0adb2da2e4115eabf44b16e4e
Garrett Stein-Seroussi <gsteinseroussi@gmail.com>
2020-08-18 21:15:37 -0400

    removed complicating files

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+// Set up MySQL connection.
+const mysql = require("mysql");
+require("dotenv").config();
+
+//using .env
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: 3306,
+  user: process.env.DB_USER,
+  password: "",
+  database: process.env.DB_DATABASE
+});
+
+// Make connection.
+connection.connect(err => {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
