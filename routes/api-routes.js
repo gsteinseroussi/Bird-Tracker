1e583a47c921ff2a5381f7d3df4f2e7e1f74c466
Dale Foskey <dalefoskey@icloud.com>
2020-08-21 18:47:07 -0400

    updated some route info
    Still not finished and not working at this current stage

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,48 +6,6 @@ var router = express.Router();
 module.exports = function(app) {
   console.log("here");
 
-  router.get("/", function(req, res) {
-    birds.all(function(data) {
-      var hbsObject = {
-        birds: data,
-      };
-      //       console.log(hbsObject);
-      //       res.render("index", hbsObject);
-      //     });
-      //   });
-      // };
-
-      // router.post("/birds", function(req, res) {
-      //   console.log(req.body);
-      //   birds.create(
-      //     ["name", "location"],
-      //     [req.body.name, req.body.location],
-      //     function(result) {
-      //       // Send back the ID of the new quote
-      //       res.json({ id: result.insertId });
-      //     }
-      //   );
-      // });
-
-      // router.put("/birds/:id", function(req, res) {
-      //   var condition = "id = " + req.params.id;
-
-      //   console.log("condition", condition);
-
-      //   birds.update(
-      //     {
-      //       location: req.body.location,
-      //     },
-      //     condition,
-      //     function(result) {
-      //       if (result.changedRows == 0) {
-      //         // If no rows were changed, then the ID must not exist, so 404
-      //         return res.status(404).end();
-      //       } else {
-      //         res.status(200).end();
-      //       }
-      //     }
-      //   );
-    });
-  });
+  
+  
 };
