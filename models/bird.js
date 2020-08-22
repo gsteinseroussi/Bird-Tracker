4bc382975c5512f29d94950a8ba673e731d8633f
Garrett Stein-Seroussi <gsteinseroussi@gmail.com>
2020-08-22 12:04:08 -0400

    update bird.js model and html-routes. Display basic handlebars.

--- models/bird.js
+++ models/bird.js
@@ -1,4 +1,4 @@
-module.exports = function (sequelize, DataTypes) {
+module.exports = (sequelize, DataTypes) => {
   const Bird = sequelize.define("Bird", {
     name: {
       type: DataTypes.STRING,
@@ -12,6 +12,26 @@ module.exports = function (sequelize, DataTypes) {
       allowNull: false,
       validate: {
         len: [1]
+      }
+    },
+    activity: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    time: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isTime(value) {
+          if (
+            value !== "Morning" ||
+            value !== "Dawn" ||
+            value !== "Dusk" ||
+            value !== "Afternoon"
+          ) {
+            throw new Error("Time must be one of the options provided");
+          }
+        }
       }
     }
   });
