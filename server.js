1f40db64569175b64a13dc08dc36756761a3cba1
Garrett Stein-Seroussi <gsteinseroussi@gmail.com>
2020-08-22 10:55:49 -0400

    adjusted api routes

--- server.js
+++ server.js
@@ -17,13 +17,11 @@ app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// routes
-app.use(routes);
-
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
-
+// routes
+// app.use(routes);
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
