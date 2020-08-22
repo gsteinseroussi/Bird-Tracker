2b5c81d0d5ea1a4482a45d337d702056de5a1287
Dale Foskey <dalefoskey@icloud.com>
2020-08-22 09:38:52 -0400

    updated connection

--- routes/mysql.js
+++ routes/mysql.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 
-.connect(function(err) {
+connection.connect(function(err) {
     if (err) throw err;
     con.query("SELECT * FROM birds", function (err, result, fields) {
       if (err) throw err;
