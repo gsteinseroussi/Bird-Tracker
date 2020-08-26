5cb2cddb3492734ecdbf27cd43de367a39a48a33
Garrett Stein-Seroussi <gsteinseroussi@gmail.com>
2020-08-25 20:58:56 -0400

    add form and modal for add bird button

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,19 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable indent */
-$("#viewAll").on("click", () => {
-    $(".popup, .popup-content").addClass("active");
-    // $.get("/api/birds", data => {
-    //     $(data).each(() => {
-    //         $("<div>")
-    //             .addClass("card")
-    //             .val(this.name);
-    //     });
-    // });
-});
+// $("#viewAll").on("click", () => {
+//     $(".popup, .popup-content").addClass("active");
+//     // $.get("/api/birds", data => {
+//     //     $(data).each(() => {
+//     //         $("<div>")
+//     //             .addClass("card")
+//     //             .val(this.name);
+//     //     });
+//     // });
+// });
 
-$(".close, .popup").on("click", () => {
-    $(".popup, .popup-content").removeClass("active");
-});
+// $(".close, .popup").on("click", () => {
+//     $(".popup, .popup-content").removeClass("active");
+// });
+
+$("#myModal").modal("toggle");
+$("#close").modal("toggle");
