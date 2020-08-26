dfe82f616ea9690afcfe9017de61dd33af3e9458
Lisa Ellison <lisaellison9@gmail.com>
2020-08-25 21:44:52 -0400

    adding FlickrAPI

--- public/js/main.js
+++ public/js/main.js
@@ -11,6 +11,8 @@ $("#saveBird").submit((event) => {
     const timeSpotted = $("#timeSpotted").val();
 
     //flickr api call here
+    const URL = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=84ecc9e2a26d8791a7b9b8b13c2be46f&text=%22" +
+        species + "%22&sort=interestingness-desc&group_id=14477233%40N00&extras=url_q&per_page=1&page=1&format=json&nojsoncallback=1";
 
 }).then((data) => {
     //create the bird object using the model
