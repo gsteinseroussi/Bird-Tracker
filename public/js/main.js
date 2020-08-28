a02753d1c9442721d27768c2758ff5916539cad1
Garrett Stein-Seroussi <gsteinseroussi@gmail.com>
2020-08-27 21:52:43 -0400

    encripted keys

--- public/js/main.js
+++ public/js/main.js
@@ -18,37 +18,31 @@ $("#addBirdForm").submit((event) => {
 
 
 
-    //flickr api call here
-    const URL = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=84ecc9e2a26d8791a7b9b8b13c2be46f&text=%22" +
-        species + "%22&sort=interestingness-desc&group_id=14477233%40N00&extras=url_q&per_page=1&page=1&format=json&nojsoncallback=1";
-
-    $.ajax(URL, {
-        type: "GET",
-    }).then((res) => {
-        console.log(res.photos.photo[0].url_q);
-        const imageURL = res.photos.photo[0].url_q;
-
-        const location = $("#location").val().trim();
-        const activity = $("#activity").val().trim();
-        const timeSpotted = $("#timeSpotted").val();
-
-        const newBird = {
-            name: species,
-            location: location,
-            activity: activity,
-            time: timeSpotted,
-            imageUrl: imageURL
-        };
 
 
-        $.ajax("/api/birds", {
-            type: "POST",
-            data: newBird
-        }).then(() => {
-            console.log("Successfully added bird");
-        });
 
+
+
+    const location = $("#location").val().trim();
+    const activity = $("#activity").val().trim();
+    const timeSpotted = $("#timeSpotted").val();
+
+    const newBird = {
+        name: species,
+        location: location,
+        activity: activity,
+        time: timeSpotted,
+    };
+
+
+    $.ajax("/api/birds", {
+        type: "POST",
+        data: newBird
+    }).then(() => {
+        console.log("Successfully added bird");
     });
+
+
     //the .then statement will end here
 });
 
